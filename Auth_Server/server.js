bb82bf75094069385f7a4e0048afbee51b4b87e4
Palmer <pzpaul2002@yahoo.com>
2014-11-08 19:42:40 -0500

    minor fixes to Auth_Server
    Added placeholders for all sign up fields. Made Sign up and Login side
    by side. Added indexing for username lookup.

--- Auth_Server/server.js
+++ Auth_Server/server.js
@@ -15,7 +15,7 @@ var server = app.listen(8000, function () {
 mongoose.connect('mongodb://test:password@ds047720.mongolab.com:47720/db_example');
 
 var userSchema = mongoose.Schema({
-    uname: String,
+    uname: {type: String, index: true},
     passwd: String,
     salt: String,
     name: {
