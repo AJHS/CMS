6117d13c6c146ac52f3b257478507b60a61c5d22
Palmer <pzpaul2002@yahoo.com>
2014-12-11 22:19:36 -0500

    Made a page for listing posts
    Proof of concept for listing blog posts of a blog
    Thanks Zachary for making this with me!

---
+++ ListPosts/js/main.js
@@ -0,0 +1,24 @@
+var blog = {
+    name: "Blog of Titles",
+    author: "John Doe"
+};
+
+var post = function (title, sub) {
+    this.title = title;
+    this.sub = sub;
+};
+
+var posts = [];
+posts.push(new post("Title", "a title"));
+posts.push(new post("Not title", "not a title"));
+posts.push(new post("Definitely not a title", "a title"));
+posts.push(new post("Title?", "no"));
+
+$(document).ready(function() {
+    $("#blogname").html("<h2>" + blog.name + "</h2><h3>by " + blog.author + "</h3>");
+
+    var $hlines = $("#hlines");
+    for (var i = 0; i < posts.length; i++) {
+        $hlines.append("<div class=\"post\"><h2><a href=\"www.google.com\">" + posts[i].title + "</a></h2><h3><em>" + posts[i].sub + "</em></h3></div>");
+    }
+});
