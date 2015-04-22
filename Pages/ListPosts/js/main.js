7b616e182bb2adaaba57f67b5cf15a37fa7da0b3
Palmer <pzpaul2002@yahoo.com>
2015-04-21 20:21:52 -0400

    Completed fixing of Pages

--- Pages/ListPosts/js/main.js
+++ Pages/ListPosts/js/main.js
@@ -1,25 +1,37 @@
-var blog = {
+$(document).ready(function () {
+  var blog = {
     name: "Blog of Titles",
-    author: "John Doe"
-};
+    author: "Cisero"
+  };
 
-var post = function (title, sub) {
+  var post = function (title, sub) {
     this.title = title;
     this.sub = sub;
-};
+  };
 
-var posts = [];
-posts.push(new post("Lorem Ipsum", "Nam ut massa convallis, feugiat augue gravida"));
-posts.push(new post("Dolor Sit", "Nam ut massa convallis, feugiat augue gravida"));
-posts.push(new post("Amet Consectetur", "Nam ut massa convallis, feugiat augue gravida"));
-posts.push(new post("Adipiscing Elit", "Nam ut massa convallis, feugiat augue gravida"));
+  var posts = [];
+  posts.push(new post("Lorem Ipsum", "Nam ut massa convallis, feugiat augue gravida"));
+  posts.push(new post("Dolor Sit", "Nam ut massa convallis, feugiat augue gravida"));
+  posts.push(new post("Amet Consectetur", "Nam ut massa convallis, feugiat augue gravida"));
+  posts.push(new post("Adipiscing Elit", "Nam ut massa convallis, feugiat augue gravida"));
 
-$(document).ready(function() {
-    $("#blogname").html("<h2>" + blog.name + "</h2><h3>by " + blog.author + "</h3>");
+  $(".page-header").html("<h2>" + blog.name + "</h2><h3>" + blog.author + "</h3>");
 
-    var $hlines = $("#hlines");
-    for (var i = 0; i < posts.length; i++) {
-        $hlines.append("<div class=\"post\"><div class=\"panel panel-default\"><h3 class=\"panel-body\">" + posts[i].title + "</h3><div class=\"panel-footer\">" + posts[i].sub + "</div></div></div>");
-    }
+  var $hlines = $("#hlines");
+  for (var i = 0; i < posts.length; i++) {
+    $hlines.append(
+      "<div class=\"panel panel-default\">" +
+        "<div class=\"panel-heading\">" +
+          "<a href=\"http://www.google.com\">" +
+            "<h2 class=\"panel-title\">" +
+              posts[i].title +
+            "</h2>" +
+          "</a>" +
+        "</div>" +
+        "<div class=\"panel-body\">" +
+          posts[i].sub +
+        "</div>" +
+      "</div>"
+    );
+  }
 });
-
